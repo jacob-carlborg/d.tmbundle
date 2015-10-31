cc30fe69f13e4428573a351a2cf631ce3c15af0c
Jacob Carlborg <doob@me.com>
2015-10-31 21:02:15 +0100

    Add white_space rule

--- Syntaxes/rules/other.tm_lang.rb
+++ Syntaxes/rules/other.tm_lang.rb
@@ -1,4 +1,9 @@
 trait :other do
+  rule 'white_space' do
+    name 'support.other.white-space.d'
+    match { one_or_more(space) }
+  end
+
   rule 'space' do
     name 'support.other.space.d'
     match '[\x{0020}\x{0009}]'
--- spec/dgrammar/rules/other_spec.rb
+++ spec/dgrammar/rules/other_spec.rb
@@ -1,6 +1,26 @@
 require 'spec_helper'
 
 describe 'other' do
+  describe 'white_space' do
+    let(:rule) { 'white_space' }
+    let(:scope) { 'support.other.white-space.d' }
+
+    describe '\u0020' do
+      subject { "\u0020" }
+      it { should be_parsed_as(scope).in_code(subject).with_rule(rule) }
+    end
+
+    describe '\u0009' do
+      subject { "\u0009" }
+      it { should be_parsed_as(scope).in_code(subject).with_rule(rule) }
+    end
+
+    describe '\u0020\u0009\u0020' do
+      subject { "\u0020\u0009\u0020" }
+      it { should be_parsed_as(scope).in_code(subject).with_rule(rule) }
+    end
+  end
+
   describe 'space' do
     let(:rule) { 'space' }
     let(:scope) { 'support.other.space.d' }
