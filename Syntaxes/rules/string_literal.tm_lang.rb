ccc4a42df1e822eced11ad31d5cc2c456bbd92a6
Jacob Carlborg <doob@me.com>
2015-10-31 21:13:19 +0100

    Add hex_string_char rule

--- Syntaxes/rules/string_literal.tm_lang.rb
+++ Syntaxes/rules/string_literal.tm_lang.rb
@@ -1,4 +1,9 @@
 trait :string_literal do
+  rule 'hex_string_char' do
+    name 'support.other.hex-string-char.d'
+    match { hex_digit | white_space }
+  end
+
   rule 'string_postfix' do
     name 'support.other.string-postfix.d'
     match /[cwd]/
--- spec/dgrammar/rules/string_literal_spec.rb
+++ spec/dgrammar/rules/string_literal_spec.rb
@@ -1,6 +1,32 @@
 require 'spec_helper'
 
 describe 'string_literal' do
+  describe 'hex_string_char' do
+    let(:rule) { 'hex_string_char' }
+    let(:scope) { 'support.other.hex-string-char.d' }
+
+    describe '0' do
+      it { should be_parsed_as(scope).in_code(subject).with_rule(rule) }
+    end
+
+    describe '3' do
+      it { should be_parsed_as(scope).in_code(subject).with_rule(rule) }
+    end
+
+    describe 'e' do
+      it { should be_parsed_as(scope).in_code(subject).with_rule(rule) }
+    end
+
+    describe 'C' do
+      it { should be_parsed_as(scope).in_code(subject).with_rule(rule) }
+    end
+
+    describe '\u0020\u0020' do
+      subject { "\u0020\u0020" }
+      it { should be_parsed_as(scope).in_code(subject).with_rule(rule) }
+    end
+  end
+
   describe 'string_postfix' do
     let(:rule) { 'string_postfix' }
     let(:scope) { 'support.other.string-postfix.d' }
