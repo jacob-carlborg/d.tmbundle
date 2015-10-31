7b46dddfc0d20c1fd0d8741b0c536e22e004436f
Jacob Carlborg <doob@me.com>
2015-10-31 21:18:00 +0100

    Add hex_string_chars rule

--- Syntaxes/rules/string_literal.tm_lang.rb
+++ Syntaxes/rules/string_literal.tm_lang.rb
@@ -1,4 +1,9 @@
 trait :string_literal do
+  rule 'hex_string_chars' do
+    name 'support.other.hex-string-chars.d'
+    match { one_or_more(hex_string_char) }
+  end
+
   rule 'hex_string_char' do
     name 'support.other.hex-string-char.d'
     match { hex_digit | white_space }
--- spec/dgrammar/rules/string_literal_spec.rb
+++ spec/dgrammar/rules/string_literal_spec.rb
@@ -1,6 +1,28 @@
 require 'spec_helper'
 
 describe 'string_literal' do
+  describe 'hex_string_chars' do
+    let(:rule) { 'hex_string_chars' }
+    let(:scope) { 'support.other.hex-string-chars.d' }
+
+    describe '0273Eec  a29CD1 92 ' do
+      it { should be_parsed_as(scope).in_code(subject).with_rule(rule) }
+    end
+
+    describe '3ad' do
+      it { should be_parsed_as(scope).in_code(subject).with_rule(rule) }
+    end
+
+    describe 'C' do
+      it { should be_parsed_as(scope).in_code(subject).with_rule(rule) }
+    end
+
+    describe '\u0020\u0020' do
+      subject { "\u0020\u0020" }
+      it { should be_parsed_as(scope).in_code(subject).with_rule(rule) }
+    end
+  end
+
   describe 'hex_string_char' do
     let(:rule) { 'hex_string_char' }
     let(:scope) { 'support.other.hex-string-char.d' }
