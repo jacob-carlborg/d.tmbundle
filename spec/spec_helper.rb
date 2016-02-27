77319ba13ba82ebc24a38f8d9f8d96a1f94d3737
Jacob Carlborg <doob@me.com>
2016-02-27 14:28:15 +0100

    Move be_parsed_as matcher to tm_grammar gem

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,18 +4,22 @@ require 'active_support/core_ext/object/blank'
 require 'nokogiri'
 require 'pry'
 
+require 'tm_grammar/rspec'
+
 DMATE_ROOT = File.expand_path(File.dirname(__FILE__) + '/..')
 $LOAD_PATH << File.join(DMATE_ROOT, 'Support/lib')
 Dir[DMATE_ROOT + '/spec/support/**/*.rb'].each { |f| require f }
 
 RSpec.configure do |config|
-  config.include DMate::Support::CodeToFile
-
   config.order = 'random'
   config.expect_with(:rspec) { |c| c.syntax = :should }
   config.mock_with(:rspec) { |c| c.syntax = :should }
 
+  config.before :suite do
+    TmGrammar::RSpec::Matchers::BeParsedAs.grammar = 'Syntaxes/d.tm_lang.rb'
+  end
+
   config.after :example do
-    DMate::Support::TempFiles.clear
+    TmGrammar::RSpec::Util::TempFiles.clear
   end
 end
