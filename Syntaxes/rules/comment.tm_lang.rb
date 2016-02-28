1cd4e1e4647a18f7f7c4e0b5bffc581e988f5cd8
Jacob Carlborg <doob@me.com>
2016-02-28 13:14:51 +0100

    Add nesting_block_comment rule

--- Syntaxes/rules/comment.tm_lang.rb
+++ Syntaxes/rules/comment.tm_lang.rb
@@ -8,4 +8,16 @@ trait :comment do
     begin_capture 0, 'punctuation.definition.comment.begin.d'
     end_capture 0, 'punctuation.definition.comment.end.d'
   end
+
+  rule 'nesting_block_comment' do
+    name 'comment.block.nesting.d'
+
+    self.begin '/\+'
+    self.end '\+/'
+
+    begin_capture 0, 'punctuation.definition.comment.begin.d'
+    end_capture 0, 'punctuation.definition.comment.end.d'
+
+    pattern { include '#nesting_block_comment' }
+  end
 end
--- spec/dgrammar/rules/comment_spec.rb
+++ spec/dgrammar/rules/comment_spec.rb
@@ -13,4 +13,23 @@ describe 'comment' do
       it { should be_parsed_as(scope).in_code(subject).with_rule(rule) }
     end
   end
+
+  describe 'nesting_block_comment' do
+    let(:rule) { 'nesting_block_comment' }
+    let(:scope) { 'comment.block.nesting.d' }
+
+    describe '/+ foo +/' do
+      it { should be_parsed_as(scope).in_code(subject).with_rule(rule) }
+    end
+
+    describe "/+\nfoo\n+/" do
+      it { should be_parsed_as(scope).in_code(subject).with_rule(rule) }
+    end
+
+    context 'nesting comments' do
+      describe "/+\nfoo /+\nbar\n+/ \n+/" do
+        it { should be_parsed_as(scope).in_code(subject).with_rule(rule) }
+      end
+    end
+  end
 end
