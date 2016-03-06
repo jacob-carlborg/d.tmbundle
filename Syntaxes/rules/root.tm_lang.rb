97ef2dfd6e664d1ff557f092079787fe9e5671c1
Jacob Carlborg <doob@me.com>
2016-03-06 17:26:38 +0100

    Add more rules to the root

--- Syntaxes/rules/root.tm_lang.rb
+++ Syntaxes/rules/root.tm_lang.rb
@@ -2,6 +2,15 @@ trait :root do
   pattern { include '#root' }
 
   rule 'root' do
+    # Grammar
     pattern { include '#module_declaration' }
+
+    # Lexical
+    pattern { include '#character_literal' }
+    pattern { include '#string_literal' }
+    pattern { include '#float_literal' }
+    pattern { include '#integer_literal' }
+    pattern { include '#keyword' }
+    pattern { include '#operator' }
   end
 end
--- spec/dgrammar/rules/root_spec.rb
+++ spec/dgrammar/rules/root_spec.rb
@@ -10,4 +10,52 @@ describe 'root' do
       it { should be_parsed_as(scope).in_code(subject).with_rule(rule) }
     end
   end
+
+  describe 'string_literal' do
+    let(:scope) { 'string.quoted.double.d' }
+
+    describe '"foo"' do
+      it { should be_parsed_as(scope).in_code(subject).with_rule(rule) }
+    end
+  end
+
+  describe 'character_literal' do
+    let(:scope) { 'string.quoted.single.d' }
+
+    describe "'a'" do
+      it { should be_parsed_as(scope).in_code(subject).with_rule(rule) }
+    end
+  end
+
+  describe 'integer_literal' do
+    let(:scope) { 'constant.numeric.integer.d' }
+
+    describe '123' do
+      it { should be_parsed_as(scope).in_code(subject).with_rule(rule) }
+    end
+  end
+
+  describe 'float_literal' do
+    let(:scope) { 'constant.numeric.float.d' }
+
+    describe '123.456' do
+      it { should be_parsed_as(scope).in_code(subject).with_rule(rule) }
+    end
+  end
+
+  describe 'keyword' do
+    let(:scope) { 'keyword.other.d' }
+
+    describe 'return' do
+      it { should be_parsed_as(scope).in_code(subject).with_rule(rule) }
+    end
+  end
+
+  describe 'operator' do
+    let(:scope) { 'keyword.operator.d' }
+
+    describe '==' do
+      it { should be_parsed_as(scope).in_code(subject).with_rule(rule) }
+    end
+  end
 end
