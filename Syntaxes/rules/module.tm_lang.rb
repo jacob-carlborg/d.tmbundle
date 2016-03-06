6876ecf71246c831a3f305372c283d07ed9ad57b
Jacob Carlborg <doob@me.com>
2016-03-06 13:10:15 +0100

    Add packages rule

--- Syntaxes/rules/module.tm_lang.rb
+++ Syntaxes/rules/module.tm_lang.rb
@@ -1,4 +1,9 @@
 trait :module do
+  rule 'packages' do
+    name 'support.other.packages.d'
+    match { package_name + zero_or_more(`\\.` + package_name) }
+  end
+
   rule 'package_name' do
     name 'support.other.package-name.d'
     match { identifier }
--- spec/dgrammar/rules/module_spec.rb
+++ spec/dgrammar/rules/module_spec.rb
@@ -1,6 +1,30 @@
 require 'spec_helper'
 
 describe 'module' do
+  describe 'packages' do
+    let(:rule) { 'packages' }
+    let(:scope) { 'support.other.packages.d' }
+
+    describe 'foo' do
+      it { should be_parsed_as(scope).in_code(subject).with_rule(rule) }
+    end
+
+    describe 'foo.bar' do
+      it { should be_parsed_as(scope).in_code(subject).with_rule(rule) }
+    end
+
+    describe 'a.b.c.d' do
+      it { should be_parsed_as(scope).in_code(subject).with_rule(rule) }
+    end
+
+    describe 'bar' do
+      let(:scope) { 'support.other.package-name.d' }
+      let(:code) { 'foo.bar' }
+
+      it { should be_parsed_as(scope).in_code(code).with_rule(rule) }
+    end
+  end
+
   describe 'package_name' do
     let(:rule) { 'package_name' }
     let(:scope) { 'support.other.package-name.d' }
