01ee21d5df62cec6a507fb439f494d80a2457ed6
Jacob Carlborg <doob@me.com>
2016-03-06 13:11:16 +0100

    Add module_name rule

--- Syntaxes/rules/module.tm_lang.rb
+++ Syntaxes/rules/module.tm_lang.rb
@@ -1,4 +1,9 @@
 trait :module do
+  rule 'module_name' do
+    name 'support.other.module-name.d'
+    match { identifier }
+  end
+
   rule 'packages' do
     name 'support.other.packages.d'
     match { package_name + zero_or_more(`\\.` + package_name) }
--- spec/dgrammar/rules/module_spec.rb
+++ spec/dgrammar/rules/module_spec.rb
@@ -1,6 +1,20 @@
 require 'spec_helper'
 
 describe 'module' do
+  describe 'module_name' do
+    let(:rule) { 'module_name' }
+    let(:scope) { 'support.other.module-name.d' }
+
+    describe 'foo' do
+      it { should be_parsed_as(scope).in_code(subject).with_rule(rule) }
+    end
+
+    describe 'foo' do
+      let(:scope) { 'support.other.identifier.d' }
+      it { should be_parsed_as(scope).in_code(subject).with_rule(rule) }
+    end
+  end
+
   describe 'packages' do
     let(:rule) { 'packages' }
     let(:scope) { 'support.other.packages.d' }
