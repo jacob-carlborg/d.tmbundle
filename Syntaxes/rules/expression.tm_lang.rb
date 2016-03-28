a843ca56a96969a69067432c1dea8d38f5319c3c
Jacob Carlborg <doob@me.com>
2016-03-28 14:40:59 +0200

    Add todos

--- Syntaxes/rules/expression.tm_lang.rb
+++ Syntaxes/rules/expression.tm_lang.rb
@@ -6,10 +6,10 @@ trait :expression do
       base_postfix_expression + /\s*\+\+/ |
       base_postfix_expression + /\s*--/ |
       base_postfix_expression
-      # base_postfix_expression . template_instance |
-      # base_postfix_expression . new_expression |
-      # base_postfix_expression -- |
-      # base_postfix_expression ( argument_listopt ) |
+      # base_postfix_expression . template_instance | TODO
+      # base_postfix_expression . new_expression | TODO
+      # base_postfix_expression -- | TODO
+      # base_postfix_expression ( argument_listopt ) | TODO
     end
   end
 
@@ -17,9 +17,9 @@ trait :expression do
     name 'support.other.base-postfix-expression.d'
     match do
       primary_expression
-      # type_ctors? + basic_type + ( argument_list? )
-      # index_expression
-      # slice_expression
+      # type_ctors? + basic_type + ( argument_list? ) TODO
+      # index_expression TODO
+      # slice_expression TODO
     end
   end
 
@@ -33,20 +33,20 @@ trait :expression do
       float_literal |
       integer_literal |
       character_literal |
-      # string_literals |
-      # array_literal |
-      # assoc_array_literal |
-      # function_literal |
-      # assert_expression |
-      # mixin_expression |
-      # import_expression |
-      # new_expression_with_args |
+      # string_literals | TODO
+      # array_literal | TODO
+      # assoc_array_literal | TODO
+      # function_literal | TODO
+      # assert_expression | TODO
+      # mixin_expression | TODO
+      # import_expression | TODO
+      # new_expression_with_args | TODO
       basic_type_x + /\s*\.\s*/ + identifier |
       typeof |
-      # typeid_expression |
-      # is_expression |
-      # ( expression ) |
-      # traits_expression |
+      # typeid_expression | TODO
+      # is_expression | TODO
+      # ( expression ) | TODO
+      # traits_expression | TODO
       keyword_variable |
       keyword_constant |
       special_keyword |
